3b7865957612bfea3d44b77e953f1fa55f314687
Leticia <leticiamalato3@gmail.com>
2026-05-05 22:01:28 -0300

    refactor: inclusao colunas

--- aa.py
+++ aa.py
@@ -3,16 +3,26 @@ import requests
 url = "https://tech-challenge-churn.onrender.com/predict"
 
 payload = {
-  "tenure": 24,
-  "monthly_charges": 75.5,
-  "total_charges": 1800.0,
-  "contract": "Month-to-month",
-  "internet_service": "Fiber optic",
-  "tech_support": "No",
-  "paperless_billing": "true",
-  "payment_method": "Electronic check"
+    "gender": "Male",               
+    "senior_citizen": "No",         
+    "partner": "No",                
+    "dependents": "No",             
+    "tenure_months": 24,            # era "tenure", corrigido para "tenure_months"
+    "monthly_charges": 75.5,
+    "total_charges": 1800.0,
+    "contract": "Month-to-month",
+    "internet_service": "Fiber optic",
+    "tech_support": "No",
+    "paperless_billing": "Yes",     # era "true", corrigido para "Yes"
+    "payment_method": "Electronic check",
+    "phone_service": "Yes",         
+    "multiple_lines": "No",         # "Yes", "No" ou "No phone service"
+    "online_security": "No",        
+    "online_backup": "No",          
+    "device_protection": "No",      
+    "streaming_tv": "No",           
+    "streaming_movies": "No",       
 }
-
 response = requests.post(url, json=payload)
 
 print(f"Status: {response.status_code}")
